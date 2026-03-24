561e3db679132dedc0815ea10d99b45ef89fe57e
Aryan Bhaskar <abhaskar9058@gmail.com>
2026-03-25 00:47:57 +0530

    first commit

---
+++ DirKam Fashion Store/backend/controller/productController.js
@@ -0,0 +1,64 @@
+import uploadOnCloudinary from "../config/cloudinary.js"
+import Product from "../model/productModel.js"
+
+
+export const addProduct = async (req,res) => {
+    try {
+        let {name,description,price,category,subCategory,sizes,bestseller} = req.body
+
+        let image1 = req.files.image1 && req.files.image1[0] ? await uploadOnCloudinary(req.files.image1[0].path) : undefined;
+        let image2 = req.files.image2 && req.files.image2[0] ? await uploadOnCloudinary(req.files.image2[0].path) : undefined;
+        let image3 = req.files.image3 && req.files.image3[0] ? await uploadOnCloudinary(req.files.image3[0].path) : undefined;
+        let image4 = req.files.image4 && req.files.image4[0] ? await uploadOnCloudinary(req.files.image4[0].path) : undefined;
+        
+        let productData = {
+            name,
+            description,
+            price :Number(price),
+            category,
+            subCategory,
+            sizes :JSON.parse(sizes),
+            bestseller :bestseller === "true" ? true : false,
+            date :Date.now(),
+            image1,
+            image2,
+            image3,
+            image4
+            
+        }
+
+        const product = await Product.create(productData)
+
+        return res.status(201).json(product)
+
+    } catch (error) {
+        console.error("AddProduct Error:", error);
+        return res.status(500).json({message:`AddProduct error ${error.message}`})
+    }
+    
+}
+
+
+export const listProduct = async (req,res) => {
+     
+    try {
+        const product = await Product.find({});
+        return res.status(200).json(product)
+
+    } catch (error) {
+        console.log("ListProduct error")
+    return res.status(500).json({message:`ListProduct error ${error}`})
+    }
+}
+
+export const removeProduct = async (req,res) => {
+    try {
+        let {id} = req.params;
+        const product = await Product.findByIdAndDelete(id)
+         return res.status(200).json(product)
+    } catch (error) {
+        console.log("RemoveProduct error")
+    return res.status(500).json({message:`RemoveProduct error ${error}`})
+    }
+    
+}
