97551ee91ff82a5c33accc0f33b685a8ec0a6d8f
Aryan Bhaskar <abhaskar9058@gmail.com>
2026-03-25 01:36:25 +0530

    Fix context import

--- DirKam Fashion Store/frontend/src/main.jsx
+++ DirKam Fashion Store/frontend/src/main.jsx
@@ -3,9 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import AuthContext from './context/authContext.jsx'
-import UserContext from './context/UserContext.jsx'
-import ShopContext from './context/ShopContext.jsx'
+import AuthContext from './context/AuthContext.
+import UserContext from './context/UserContext.
+import ShopContext from './context/ShopContext.
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
